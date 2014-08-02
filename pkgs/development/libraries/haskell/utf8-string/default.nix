c64a3fd0bca8539258439b6f784fd5ecb2d170d8
Ian-Woo Kim <ianwookim@gmail.com>
2014-08-02 18:31:57 +0200

    noHaddock for utf8-string

--- pkgs/development/libraries/haskell/utf8-string/default.nix
+++ pkgs/development/libraries/haskell/utf8-string/default.nix
@@ -6,7 +6,7 @@ cabal.mkDerivation (self: {
   pname = "utf8-string";
   version = "0.3.8";
   sha256 = "1h29dn0scsfkhmkg14ywq9178lw40ah1r36w249zfzqr02y7qxc0";
-  noHaddock = self.stdenv.lib.versionOlder self.ghc.version "6.11";
+  noHaddock = self.stdenv.lib.versionOlder self.ghc.version "6.11" || self.stdenv.isDarwin;
   patchPhase = ''
     sed -ir -e 's|Extensions: | Extensions: UndecidableInstances, |' utf8-string.cabal
   '';
