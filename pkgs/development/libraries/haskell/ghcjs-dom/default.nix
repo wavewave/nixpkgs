14113554f31779b21fdf091166d6e8afd6cbd8aa
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-01-31 13:57:16 -0500

    Fix ghcjs-dom on regular ghc

--- pkgs/development/libraries/haskell/ghcjs-dom/default.nix
+++ pkgs/development/libraries/haskell/ghcjs-dom/default.nix
@@ -3,8 +3,8 @@
 , ghcjsBase ? null # jsffi dependencies
 , glib ? null, transformers ? null, gtk ? null, webkit ? null # webkit dependencies
 }:
-
-cabal.mkDerivation (self: {
+let configureFlags = if buildType == "jsffi" then [ ] else if buildType == "webkit" then [ "-f-ghcjs" "-fwebkit" "-f-gtk3" ] else throw "unrecognized buildType";
+in cabal.mkDerivation (self: {
   pname = "ghcjs-dom";
   version = "0.1.1.3";
   src = fetchgit {
@@ -13,7 +13,9 @@ cabal.mkDerivation (self: {
     sha256 = "949a661516206252b36dc817c07ababb7f50a389661d838f0fee0be15085aff9";
   };
   buildDepends = [ mtl ] ++ (if buildType == "jsffi" then [ ghcjsBase ] else if buildType == "webkit" then [ glib transformers gtk webkit ] else throw "unrecognized buildType");
-  configureFlags = if buildType == "jsffi" then [ ] else if buildType == "webkit" then [ "-f-ghcjs" "-fwebkit" "-f-gtk3" ] else throw "unrecognized buildType";
+  preConfigure = ''
+    configureFlags="${self.stdenv.lib.concatStringsSep " " configureFlags} $configureFlags"
+  '';
   meta = {
     description = "DOM library that supports both GHCJS and WebKitGTK";
     license = self.stdenv.lib.licenses.mit;
