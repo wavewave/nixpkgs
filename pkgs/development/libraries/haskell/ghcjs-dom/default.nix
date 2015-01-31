c509e721f822cd7965a2218b65c7b814d7ac1ae3
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-01-31 13:35:12 -0500

    Revert "ghcjs-dom: clean up"
    This reverts commit 0dabcc34c3f0568893af485cbc8dd55c4c70ad46.
    
    Conflicts:
    	pkgs/development/libraries/haskell/ghcjs-dom/default.nix

--- pkgs/development/libraries/haskell/ghcjs-dom/default.nix
+++ pkgs/development/libraries/haskell/ghcjs-dom/default.nix
@@ -1,4 +1,8 @@
-{ cabal, fetchgit, ghcjsBase, mtl, text }:
+{ cabal, fetchgit, ghc, mtl
+, buildType ? if ghc.ghc.pname or null == "ghcjs" then "jsffi" else "webkit"
+, ghcjsBase ? null # jsffi dependencies
+, glib ? null, transformers ? null, gtk ? null, webkit ? null # webkit dependencies
+}:
 
 cabal.mkDerivation (self: {
   pname = "ghcjs-dom";
@@ -8,7 +12,8 @@ cabal.mkDerivation (self: {
     rev = "90b52182ab3d2ca5cbed7b2fbb33fa06f280771e";
     sha256 = "949a661516206252b36dc817c07ababb7f50a389661d838f0fee0be15085aff9";
   };
-  buildDepends = [ ghcjsBase mtl text ];
+  buildDepends = [ mtl ] ++ (if buildType == "jsffi" then [ ghcjsBase ] else if buildType == "webkit" then [ glib transformers gtk webkit ] else throw "unrecognized buildType");
+  configureFlags = if buildType == "jsffi" then [ ] else if buildType == "webkit" then [ "-f-ghcjs" "-fwebkit" "-f-gtk3" ] else throw "unrecognized buildType";
   meta = {
     description = "DOM library that supports both GHCJS and WebKitGTK";
     license = self.stdenv.lib.licenses.mit;
