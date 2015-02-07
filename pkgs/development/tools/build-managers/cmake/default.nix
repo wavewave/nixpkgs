e40fef3a0a61b41dceedf42dba4887a279880993
Joel Taylor <me@joelt.io>
2015-02-06 21:22:04 -0800

    set curses paths on Darwin

--- pkgs/development/tools/build-managers/cmake/default.nix
+++ pkgs/development/tools/build-managers/cmake/default.nix
@@ -42,6 +42,9 @@ stdenv.mkDerivation rec {
   postPatch = ''
     substituteInPlace Modules/Platform/Darwin.cmake \
       --replace 'list(REMOVE_DUPLICATES _apps_paths)' ''$'if (_apps_paths)\nlist(REMOVE_DUPLICATES _apps_paths)\nendif()'
+  '' + stdenv.lib.optionalString stdenv.isDarwin ''
+    echo 'set(CURSES_CURSES_LIBRARY "${ncurses}/lib/libncursesw.dylib")' >> CompileFlags.cmake
+    echo 'set(CURSES_CURSES_H_PATH "${ncurses}/include")' >> CompileFlags.cmake
   '';
 
   buildInputs = [ curl expat zlib bzip2 libarchive ]
