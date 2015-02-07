b40cb02a5cbf1516d7098eac9814cc969bd62bba
Dan Peebles <thetypesaretoobig@gmail.com>
2015-02-07 15:44:11 -0500

    Add some metadata for openbsm

--- pkgs/development/libraries/openbsm/default.nix
+++ pkgs/development/libraries/openbsm/default.nix
@@ -7,4 +7,11 @@ stdenv.mkDerivation {
     url    = "http://www.trustedbsd.org/downloads/openbsm-1.1-p2.tgz";
     sha256 = "1cwyxvl9mpssnfkq6cjblc2ssa91s2ar5zz9g1n6mfvfs0kmlf7k";
   };
+
+  meta = with stdenv.lib; {
+    description = "A portable implementation of Sun's Basic Security Module (BSM) security audit API and file format";
+    license     = licenses.bsd2;
+    platforms   = platforms.linux ++ platforms.darwin;
+    maintainers = with maintainers; [ copumpkin ];
+  };
 }
