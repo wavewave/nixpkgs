b2e553511ecb4421492110d1788e2502c6c2ad9e
Ian-Woo Kim <ianwookim@gmail.com>
2015-02-15 06:19:29 +0100

    sw_vers: more complete mock-up

--- pkgs/os-specific/darwin/sw_vers/default.nix
+++ pkgs/os-specific/darwin/sw_vers/default.nix
@@ -7,12 +7,19 @@ stdenv.mkDerivation {
     cat >$out/bin/sw_vers <<EOF
     #!${stdenv.shell}
     if test "\$#" -eq 0 ;
-      then 
-        echo "ProductName:    Mac OS X"
-        echo "ProductVersion: 10.9.4"
-        echo "BuildVersion:   13E28"
-      else
+    then 
+      echo "ProductName:    Mac OS X"
+      echo "ProductVersion: 10.9.4"
+      echo "BuildVersion:   13E28"
+    elif [ "\$1" = "-ProductName" ];
+    then
+        echo "Mac OS X"
+    elif [ "\$1" = "-ProductVersion" ];
+    then
         echo "10.9.4"
+    elif [ "\$1" = "-BuildVersion" ];
+    then
+        echo "13E28"
     fi
     EOF
     chmod a+x $out/bin/sw_vers
