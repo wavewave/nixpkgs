e830b373993ddc5ddf0404d766bf20e7c35e3c6c
John Ericson <Ericson2314@Yahoo.com>
2017-06-15 14:38:34 -0400

    cctools-port: Use old version for cross to get around regression

--- pkgs/os-specific/darwin/cctools/port-old.nix
+++ pkgs/os-specific/darwin/cctools/port-old.nix
@@ -2,9 +2,7 @@
 , llvm, libcxx, libcxxabi, clang, libuuid
 , libobjc ? null, maloader ? null, xctoolchain ? null
 , buildPlatform, hostPlatform, targetPlatform
-} @ args:
-
-if targetPlatform != hostPlatform then import ./port-old.nix args else
+}:
 
 let
   # The prefix prepended to binary names to allow multiple binuntils on the
@@ -22,13 +20,13 @@ assert (!hostPlatform.isDarwin) -> (maloader != null && xctoolchain != null);
 let
   baseParams = rec {
     name = "${prefix}cctools-port-${version}";
-    version = "895";
+    version = "886";
 
     src = fetchFromGitHub {
       owner  = "tpoechtrager";
       repo   = "cctools-port";
-      rev    = "2e569d765440b8cd6414a695637617521aa2375b"; # From branch 895-ld64-274.2
-      sha256 = "0l45mvyags56jfi24rawms8j2ihbc45mq7v13pkrrwppghqrdn52";
+      rev    = "02f0b8ecd87a3951653d838a321ae744815e21a5";
+      sha256 = "0bzyabzr5dvbxglr74d0kbrk2ij5x7s5qcamqi1v546q1had1wz1";
     };
 
     nativeBuildInputs = [ autoconf automake libtool_2 ];
@@ -39,6 +37,10 @@ let
 
     patches = [
       ./ld-rpath-nonfinal.patch ./ld-ignore-rpath-link.patch
+    ] ++ stdenv.lib.optionals stdenv.isDarwin [
+      # See https://github.com/tpoechtrager/cctools-port/issues/24. Remove when that's fixed.
+      ./undo-unknown-triple.patch
+      ./ld-tbd-v2.patch
     ];
 
     enableParallelBuilding = true;
@@ -111,8 +113,8 @@ let
     };
 
     meta = {
-      homepage = http://www.opensource.apple.com/source/cctools/;
-      description = "MacOS Compiler Tools (cross-platform port)";
+      homepage = "http://www.opensource.apple.com/source/cctools/";
+      description = "Mac OS X Compiler Tools (cross-platform port)";
       license = stdenv.lib.licenses.apsl20;
     };
   };
