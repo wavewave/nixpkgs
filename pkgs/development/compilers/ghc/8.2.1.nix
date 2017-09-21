ccaa90676bd27acbb2189c9ed887546de097f488
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-09-20 22:38:36 -0400

    ghc-8.2.1: use integer-simple when cross-compiling for android

--- pkgs/development/compilers/ghc/8.2.1.nix
+++ pkgs/development/compilers/ghc/8.2.1.nix
@@ -15,7 +15,7 @@
 
 , # If enabled, GHC will be build with the GPL-free but slower integer-simple
   # library instead of the faster but GPLed integer-gmp library.
-  enableIntegerSimple ? false, gmp ? null
+  enableIntegerSimple ? targetPlatform.useAndroidPrebuilt or false, gmp ? null
 
 , # If enabled, use -fPIC when compiling static libs.
   enableRelocatedStaticLibs ? targetPlatform != hostPlatform
